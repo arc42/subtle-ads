49a3d17a9bfd1d87c5f3c3ac4e562823877453f0
Dr. Gernot Starke <gs@gernotstarke.de>
2017-08-04 19:33:32 +0200

    removed unwanted file, improved script

--- update-submodules-in-container.sh
+++ update-submodules-in-container.sh
@@ -1,24 +1,32 @@
 #! /bin/bash
 
-echo off
+faqsite="faq.arc42.org-site"
+docsite="docs.arc42.org-site"
+patternsite="patterns.arc42.org-site"
+mainsite="arc42.org-site"
+
+sites=( $faqsite $docsite $patternsite $mainsite)
+
+clear
 echo
-echo "arc42 Docker container to update subtle-ads on all arc42.org websites"
-echo "====================================================================="
+echo "arc42 Docker container to update subtle-ads on the arc42.org websites:"
+echo "======================================================================"
+echo
+for i in ${sites[@]}; do echo $i; done
+echo "======================================================================"
 echo
 
 echo "I need github credentials to push changes. In case you copy/paste,"
 echo "please use the paste-command from the context menu of your terminal."
 echo
-read -p "Enter github username (with push access to arc42 repositories) :" username
-read -p "Enter github credentials (personal access token) :" credential
+read -p "Enter github username (with push access to arc42 repositories) : " username
+read -p "Enter github credentials (personal access token) : " credential
 
 authuser=$username:$credential
+
 echo "Thanx. Now using $authuser to authenticate at github."
 echo
 
-faqsite="faq.arc42.org-site"
-docsite="docs.arc42.org-site"
-sites=( faqsite docsite )
 
 dirname="tmp-"$(date +%Y-%m-%d)
 mkdir $dirname
@@ -33,16 +41,19 @@ echo "$dirname" created
 function update_submodule() {
    echo
    echo "updating $1"
-   echo "================="
-   #git clone --recursive https://github.com/arc42/$1
+   echo "========================================="
+   echo "git clone..."
+   git clone --recursive https://github.com/arc42/$1
    cd $1
+   echo "update submodule..."
    git submodule update --remote
    git add .
+   echo "commit..."
    git commit -m "updated subtle-ads $(date +%Y-%m-%d) from Docker container"
+   echo "push to Github"
    git push https://$authuser@github.com/arc42/$1.git
    cd ..
 }
-
 
 
 for i in "${sites[@]}"
@@ -50,9 +61,5 @@ do
 	update_submodule $i
 done
 
-
-#echo "updating patterns.arc42.org-site"
-
-#echo "updating arc42.org-site"
 
 echo $'...done'
